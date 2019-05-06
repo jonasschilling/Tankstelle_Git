75c170d7b029168c0fada7ac3d9517a3d0ee80f5
D074024 <D074024@WDFN34367201A.emea.global.corp.sap>
2019-05-06 10:51:19 +0200

    +++

--- src/litreclock/Controller.java
+++ src/litreclock/Controller.java
@@ -17,8 +17,11 @@ public class Controller implements Initializable {
 	int milliLitres = 1;
 	int litres = 0;
 
-	float amountRefilled, pricePerLitre = 1.539f, priceComp, priceCompRound;
+	float amountRefilled, pricePerLitre = 1.339f, priceComp, priceCompRound;
 
+	
+	String milliLitresText;
+	String litresText;
 	String milliLitresText0;
 
 	@FXML
@@ -38,35 +41,35 @@ public class Controller implements Initializable {
 
 	public void initialize(URL location, ResourceBundle resources) {
 
-		pricePerLitreLabel.setText(String.valueOf(pricePerLitre) + " €");
+		pricePerLitreLabel.setText(String.valueOf(getPricePerLitre()) + " €");
 
 		timer = new Timeline(new KeyFrame(Duration.millis(10), (actionEvent) -> {
-			String milliLitresText = String.valueOf(milliLitres + 1);
-			if (milliLitres < 9) {
-				milliLitresText0 = ("0" + milliLitresText);
-				milliLitresLabel.setText(milliLitresText0);
+			milliLitresText = String.valueOf(milliLitres);
+			if (milliLitres < 10) {
+				milliLitresText0 = ("0" + getMilliLitresText());
+				milliLitresLabel.setText(getMilliLitresText0());
 				if (milliLitres == 0) {
 					litres++;
 					milliLitres++;
-				} else if (milliLitres < 98) {
+				} else if (milliLitres < 99) {
 					milliLitres++;
-				} else if (milliLitres == 98) {
+				} else if (milliLitres == 99) {
 					milliLitres = 0;
 				}
 			} else {
-				milliLitresLabel.setText(milliLitresText);
+				milliLitresLabel.setText(getMilliLitresText());
 				if (milliLitres == 0) {
 					litres++;
 					milliLitres++;
-				} else if (milliLitres < 98) {
+				} else if (milliLitres < 99) {
 					milliLitres++;
-				} else if (milliLitres == 98) {
+				} else if (milliLitres == 99) {
 					milliLitres = 0;
 				}
 			}
 
-			String litresText = String.valueOf(litres);
-			litresLabel.setText(litresText);
+			litresText = String.valueOf(litres);
+			litresLabel.setText(getLitresText());
 
 			if (milliLitres < 9) {
 				setAmountRefilled(litres, milliLitresText0);
@@ -75,17 +78,68 @@ public class Controller implements Initializable {
 			}
 
 			priceComp = getAmountRefilled() * pricePerLitre;
-			
-			
-			priceCompRound = Math.round(priceComp * 100) / 100.0f;
-			priceCompLabel.setText(String.valueOf(priceCompRound) + " €");
+
+			setPriceCompRound(Math.round(priceComp * 100) / 100.0f);
+			priceCompLabel.setText(String.valueOf(getPriceCompRound()) + " €");
 
 		}));
 		timer.setCycleCount(Timeline.INDEFINITE);
 	}
 
+	public float getPricePerLitre() {
+		return pricePerLitre;
+	}
+
+	public void setPricePerLitre(float pricePerLitre) {
+		this.pricePerLitre = pricePerLitre;
+	}
+
+	public float getPriceCompRound() {
+		return priceCompRound;
+	}
+
+	public void setPriceCompRound(float priceCompRound) {
+		this.priceCompRound = priceCompRound;
+	}
+
+	public int getMilliLitres() {
+		return milliLitres;
+	}
+
+	public void setMilliLitres(int milliLitres) {
+		this.milliLitres = milliLitres;
+	}
+
+	public int getLitres() {
+		return litres;
+	}
+
+	public void setLitres(int litres) {
+		this.litres = litres;
+	}
+
+	public void setMilliLitresText0(String milliLitresText0) {
+		this.milliLitresText0 = milliLitresText0;
+	}
+
 	public String getMilliLitresText0() {
 		return milliLitresText0;
+	}
+
+	public String getMilliLitresText() {
+		return milliLitresText;
+	}
+
+	public void setMilliLitresText(String milliLitresText) {
+		this.milliLitresText = milliLitresText;
+	}
+
+	public String getLitresText() {
+		return litresText;
+	}
+
+	public void setLitresText(String litresText) {
+		this.litresText = litresText;
 	}
 
 	public void setAmountRefilled(int litres, String milliLitres) {
@@ -104,6 +158,18 @@ public class Controller implements Initializable {
 	public void stopTimer(ActionEvent actionEvent) {
 		timer.stop();
 		System.out.println(amountRefilled + " Liter für " + priceCompRound + " € getankt.");
+	}
+
+	public void resetTimer(ActionEvent actionEvent) {	
+		setLitres(0);
+		setLitresText("0");
+		setMilliLitres(1);
+		setMilliLitresText("00");
+		setMilliLitresText0("00");
+		
+		litresLabel.setText("0");
+		milliLitresLabel.setText("00");
+		priceCompLabel.setText("0.00 €");
 	}
 
 }
