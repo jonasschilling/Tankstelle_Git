2012c4a1d8988f54e71b4cdb74ebfe3f3ce028b6
D074024 <D074024@WDFN34367201A.emea.global.corp.sap>
2019-05-06 14:12:35 +0200

    ******

--- src/refillSimulation/SimulationStart.java
+++ src/refillSimulation/SimulationStart.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class SimulationStart extends Application {
@@ -19,6 +20,7 @@ public class SimulationStart extends Application {
 		Scene scene = new Scene(root, 400, 330);
 		scene.getStylesheets().add(getClass().getResource("SimulationStylesheet.css").toExternalForm());
 		primaryStage.setTitle("Tanksimulation");
+		primaryStage.getIcons().add(new Image("javafx/resources/gasPump.png"));
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
