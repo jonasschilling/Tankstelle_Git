56652babff2dddf8b5018b9087716f24b973a482
D074024 <D074024@WDFN34367201A.emea.global.corp.sap>
2019-05-07 08:03:52 +0200

    Fiori Theme

--- src/javafx/SalesController.java
+++ src/javafx/SalesController.java
@@ -1,6 +1,7 @@
 package javafx;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -9,7 +10,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.ToggleButton;
+import tanken_test.Tank;
 
 public class SalesController implements Initializable {
 
@@ -46,10 +49,32 @@ public class SalesController implements Initializable {
 	Label label5, label5amount;
 	@FXML
 	Label labelTotal;
+	@FXML
+	Label tankNameLabel1, tankNameLabel2;
+	@FXML
+	Label superPriceLabel, dieselPriceLabel;
+	@FXML
+	Label superCapLabel, dieselCapLabel;
+	@FXML
+	Button changePriceButton;
+	
+	tanken_test.Tank superTank = new Tank("Super", 12000.0f);
+	tanken_test.Tank dieselTank = new Tank("Diesel", 8000.0f);
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		scart = new Shoppingcart();
 		hideAll();
+		
+		tankNameLabel1.setText(superTank.getDescription());
+		tankNameLabel2.setText(dieselTank.getDescription());
+		
+		superTank.setPricePerLitre(1.539f);
+		dieselTank.setPricePerLitre(1.289f);
+		superPriceLabel.setText(String.valueOf(superTank.getPricePerLitre()) + " €/L");
+		dieselPriceLabel.setText(String.valueOf(dieselTank.getPricePerLitre()) + " €/L");
+		
+		superCapLabel.setText(String.valueOf(superTank.getAbsFuelLevel() + "/" + superTank.getCapacity()) + " L");
+		dieselCapLabel.setText(String.valueOf(dieselTank.getAbsFuelLevel() + "/" + dieselTank.getCapacity()) + " L");
 		
 	}
 
@@ -303,6 +328,15 @@ public class SalesController implements Initializable {
 		scart.checkout();
 		hideAll();
 		unToggleAll();
+	}
+	
+	public void showChangePriceDialog(ActionEvent actionEvent) {
+		TextInputDialog newSuperPrice = new TextInputDialog();
+		newSuperPrice.setTitle("Preise ändern");
+		newSuperPrice.setHeaderText("Preise ändern");
+		newSuperPrice.setContentText("Neuer Preis für Super eingeben: ");
+		Optional<String> input = newSuperPrice.showAndWait();
+		input.ifPresent(text -> superTank.setPricePerLitre(Float.parseFloat(text)));
 	}
 }
 
