222c22cacbbd96883162acffbf821ad25ce831c0
D073551 <D073551@WDFN34367200A.emea.global.corp.sap>
2019-05-18 20:44:54 +0200

    ...

--- src/javafx/SalesController.java
+++ src/javafx/SalesController.java
@@ -70,11 +70,6 @@ public class SalesController implements Initializable {
 		anchor3.setVisible(false);
 		anchor4.setVisible(false);
 		anchor5.setVisible(false);
-		label1.setVisible(false);
-		label2.setVisible(false);
-		label3.setVisible(false);
-		label4.setVisible(false);
-		label5.setVisible(false);
 //		label1amount.setVisible(false);
 //		label2amount.setVisible(false);
 //		label3amount.setVisible(false);
@@ -110,88 +105,39 @@ public class SalesController implements Initializable {
 		case "button6":
 
 			if (source.isSelected()) {
+				wodkaPressed();
 
-				scart.addWodka();
-				anchor1.setVisible(true);
-				label1.setVisible(true);
-				label1amount.setText("1");
-				prod1plus.setDisable(false);
 			} else {
-				label1.setVisible(false);
-				int amount1 = Integer.valueOf(label1amount.getText());
-				for (int i = 0; i < amount1; i++) {
-					scart.removeWodka();
-				}
-				label1amount.setText("0");
-				anchor1.setVisible(false);
+				wodkaReleased();
 			}
 			break;
 		case "button7":
 			if (source.isSelected()) {
-				scart.addFilip();
-				label2.setVisible(true);
-				anchor2.setVisible(true);
-				label2amount.setText("1");
-				prod2plus.setDisable(false);
+				filipPressed();
+
 			} else {
-				label2.setVisible(false);
-				int amount2 = Integer.valueOf(label2amount.getText());
-				for (int i = 0; i < amount2; i++) {
-					scart.removeFilip();
-				}
-				anchor2.setVisible(false);
-				label2amount.setText("0");
+				filipReleased();
 			}
 			break;
 		case "button8":
 			if (source.isSelected()) {
-				scart.addJupiter();
-				label3.setVisible(true);
-				anchor3.setVisible(true);
-				label3amount.setText("1");
-				prod3plus.setDisable(false);
+				jupiterPressed();
 			} else {
-				label3.setVisible(false);
-				int amount3 = Integer.valueOf(label3amount.getText());
-				for (int i = 0; i < amount3; i++) {
-					scart.removeJupiter();
-				}
-				anchor3.setVisible(false);
-				label3amount.setText("0");
+				jupiterReleased();
 			}
 			break;
 		case "button9":
 			if (source.isSelected()) {
-				scart.addBull();
-				label4.setVisible(true);
-				anchor4.setVisible(true);
-				label4amount.setText("1");
-				prod4plus.setDisable(false);
+				bullPressed();
 			} else {
-				label4.setVisible(false);
-				int amount4 = Integer.valueOf(label4amount.getText());
-				for (int i = 0; i < amount4; i++) {
-					scart.removeBull();
-				}
-				anchor4.setVisible(false);
-				label4amount.setText("0");
+				bullReleased();
 			}
 			break;
 		case "button10":
 			if (source.isSelected()) {
-				scart.addPizza();
-				label5.setVisible(true);
-				anchor5.setVisible(true);
-				label5amount.setText("1");
-				prod5plus.setDisable(false);
+				pizzaPressed();
 			} else {
-				label5.setVisible(false);
-				int amount5 = Integer.valueOf(label5amount.getText());
-				for (int i = 0; i < amount5; i++) {
-					scart.removePizza();
-				}
-				anchor5.setVisible(false);
-				label5amount.setText("0");
+				pizzaReleased();
 			}
 			break;
 		}
@@ -201,15 +147,20 @@ public class SalesController implements Initializable {
 	public void addButtonClicked(ActionEvent actionEvent) {
 
 		Button source = (Button) actionEvent.getSource();
+		Label currentlabel;
+		float price;
 		switch (source.getId()) {
 
 		case ("prod1plus"):
-
+			
 			int amount1 = Integer.valueOf(label1amount.getText());
 			amount1++;
 			label1amount.setText(String.valueOf(amount1));
 			scart.addWodka();
 			prod1plus.setDisable(scart.getNumWodka() == (scart.wodka.getAmount()) ? true : false);
+			currentlabel = getCurrentLabel("Wodka");
+			price = scart.getNumWodka() * scart.wodka.getPriceSell();
+			currentlabel.setText("Wodka                 " + price + "€");
 			break;
 		case ("prod2plus"):
 			int amount2 = Integer.valueOf(label2amount.getText());
@@ -217,6 +168,9 @@ public class SalesController implements Initializable {
 			label2amount.setText(String.valueOf(amount2));
 			scart.addFilip();
 			prod2plus.setDisable(scart.getNumFilip() == (scart.filip.getAmount()) ? true : false);
+			currentlabel = getCurrentLabel("Filip");
+			price = scart.getNumFilip() * scart.filip.getPriceSell();
+			currentlabel.setText("Filip Maurice        " + price + "€");
 			break;
 		case ("prod3plus"):
 			int amount3 = Integer.valueOf(label3amount.getText());
@@ -224,6 +178,9 @@ public class SalesController implements Initializable {
 			label3amount.setText(String.valueOf(amount3));
 			scart.addJupiter();
 			prod3plus.setDisable(scart.getNumJupiter() == (scart.jupiter.getAmount()) ? true : false);
+			currentlabel = getCurrentLabel("Jupiter");
+			price = scart.getNumJupiter() * scart.jupiter.getPriceSell();
+			currentlabel.setText("Jupiter Riegel        " + price + "€");
 			break;
 		case ("prod4plus"):
 			int amount4 = Integer.valueOf(label4amount.getText());
@@ -231,6 +188,9 @@ public class SalesController implements Initializable {
 			label4amount.setText(String.valueOf(amount4));
 			scart.addBull();
 			prod4plus.setDisable(scart.getNumBull() == (scart.bull.getAmount()) ? true : false);
+			currentlabel = getCurrentLabel("Bull");
+			price = scart.getNumBull() * scart.bull.getPriceSell();
+			currentlabel.setText("Sitting Bull           " + price + "€");
 			break;
 		case ("prod5plus"):
 			int amount5 = Integer.valueOf(label5amount.getText());
@@ -238,6 +198,9 @@ public class SalesController implements Initializable {
 			label5amount.setText(String.valueOf(amount5));
 			scart.addPizza();
 			prod5plus.setDisable(scart.getNumPizza() == (scart.pizza.getAmount()) ? true : false);
+			currentlabel = getCurrentLabel("Pizza");
+			price = scart.getNumPizza() * scart.pizza.getPriceSell();
+			currentlabel.setText("Pizza                    " + price + "€");
 			break;
 		}
 		labelTotal.setText("Gesamtpreis: " + scart.getTotal() + " €");
@@ -308,5 +271,266 @@ public class SalesController implements Initializable {
 		scart.cancel();
 		hideAll();
 		unToggleAll();
+	}
+
+	// die Pressed und Released-Methoden führen Operationen aus, die bei Auswählen
+	// bzw. Abwählen eines Produktes wichtig sind.
+	public void wodkaPressed() {
+		scart.addWodka();
+		anchor1.setVisible(true);
+		label1amount.setText("1");
+		prod1plus.setDisable(false);
+		Label freelabel = freeLabel();
+		float price = scart.getNumWodka() * scart.wodka.getPriceSell();
+		freelabel.setText("Wodka                 " + price + "€");
+	}
+
+	public void wodkaReleased() {
+		int amount1 = Integer.valueOf(label1amount.getText());
+		for (int i = 0; i < amount1; i++) {
+			scart.removeWodka();
+		}
+		label1amount.setText("0");
+		anchor1.setVisible(false);
+		removeWodka();
+	}
+
+	public void filipPressed() {
+		scart.addFilip();
+		anchor2.setVisible(true);
+		label2amount.setText("1");
+		prod2plus.setDisable(false);
+		Label freelabel = freeLabel();
+		float price = scart.getNumFilip() * scart.filip.getPriceSell();
+		freelabel.setText("Filip Maurice        " + price + "€");
+
+	}
+
+	public void filipReleased() {
+		int amount2 = Integer.valueOf(label2amount.getText());
+		for (int i = 0; i < amount2; i++) {
+			scart.removeFilip();
+		}
+		anchor2.setVisible(false);
+		label2amount.setText("0");
+		removeFilip();
+
+	}
+
+	public void jupiterPressed() {
+		scart.addJupiter();
+		anchor3.setVisible(true);
+		label3amount.setText("1");
+		prod3plus.setDisable(false);
+		Label freelabel = freeLabel();
+		float price = scart.getNumJupiter() * scart.jupiter.getPriceSell();
+		freelabel.setText("Jupiter Riegel        " + price + "€");
+	}
+
+	public void jupiterReleased() {
+		int amount3 = Integer.valueOf(label3amount.getText());
+		for (int i = 0; i < amount3; i++) {
+			scart.removeJupiter();
+		}
+		anchor3.setVisible(false);
+		label3amount.setText("0");
+		removeJupiter();
+	}
+
+	public void bullPressed() {
+
+		scart.addBull();
+		anchor4.setVisible(true);
+		label4amount.setText("1");
+		prod4plus.setDisable(false);
+		Label freelabel = freeLabel();
+		float price = scart.getNumBull() * scart.bull.getPriceSell();
+		freelabel.setText("Sitting Bull           " + price + "€");
+	}
+
+	public void bullReleased() {
+		int amount4 = Integer.valueOf(label4amount.getText());
+		for (int i = 0; i < amount4; i++) {
+			scart.removeBull();
+		}
+		anchor4.setVisible(false);
+		label4amount.setText("0");
+		removeBull();
+	}
+
+	public void pizzaPressed() {
+		scart.addPizza();
+		anchor5.setVisible(true);
+		label5amount.setText("1");
+		prod5plus.setDisable(false);
+		Label freelabel = freeLabel();
+		float price = scart.getNumPizza() * scart.pizza.getPriceSell();
+		freelabel.setText("Pizza                    " + price + "€");
+
+	}
+
+	public void pizzaReleased() {
+		int amount5 = Integer.valueOf(label5amount.getText());
+		for (int i = 0; i < amount5; i++) {
+			scart.removePizza();
+		}
+		anchor5.setVisible(false);
+		label5amount.setText("0");
+		removePizza();
+	}
+
+	// Die Remove-methoden lassen die Schriftzug des Produkts verschwinden und die
+	// anderen Schriftzüge weiter hoch rücken in der Anzeige
+	public void removeWodka() {
+		if (label1.getText().contains("Wodka")) {
+			label1.setText(label2.getText());
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label2.getText().contains("Wodka")) {
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label3.getText().contains("Wodka")) {
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label4.getText().contains("Wodka")) {
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label5.getText().contains("Wodka")) {
+			label5.setText("");
+		}
+	}
+
+	public void removeFilip() {
+		if (label1.getText().contains("Filip")) {
+			label1.setText(label2.getText());
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label2.getText().contains("Filip")) {
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label3.getText().contains("Filip")) {
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label4.getText().contains("Filip")) {
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label5.getText().contains("Filip")) {
+			label5.setText("");
+		}
+
+	}
+
+	public void removeJupiter() {
+		if (label1.getText().contains("Jupiter")) {
+			label1.setText(label2.getText());
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label2.getText().contains("Jupiter")) {
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label3.getText().contains("Jupiter")) {
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label4.getText().contains("Jupiter")) {
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label5.getText().contains("Jupiter")) {
+			label5.setText("");
+		}
+	}
+
+	public void removeBull() {
+		if (label1.getText().contains("Bull")) {
+			label1.setText(label2.getText());
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label2.getText().contains("Bull")) {
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label3.getText().contains("Bull")) {
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label4.getText().contains("Bull")) {
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label5.getText().contains("Bull")) {
+			label5.setText("");
+		}
+	}
+
+	public void removePizza() {
+		if (label1.getText().contains("Pizza")) {
+			label1.setText(label2.getText());
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label2.getText().contains("Pizza")) {
+			label2.setText(label3.getText());
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label3.getText().contains("Pizza")) {
+			label3.setText(label4.getText());
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label4.getText().contains("Pizza")) {
+			label4.setText(label5.getText());
+			label5.setText("");
+		} else if (label5.getText().contains("Pizza")) {
+			label5.setText("");
+		}
+	}
+
+	public Label freeLabel() {
+		if (label1.getText().equals("")) {
+			return label1;
+		} else if (label2.getText().equals("")) {
+			return label2;
+		} else if (label3.getText().equals("")) {
+			return label3;
+		} else if (label4.getText().equals("")) {
+			return label4;
+		} else if (label5.getText().equals("")) {
+			return label5;
+		} else {
+			return null;
+		}
+	}
+
+	public Label getCurrentLabel(String s) {
+		if (label1.getText().contains(s)) {
+			return label1;
+		} else if (label2.getText().contains(s)) {
+			return label2;
+		} else if (label3.getText().contains(s)) {
+			return label3;
+		} else if (label4.getText().contains(s)) {
+			return label4;
+		} else if (label5.getText().contains(s)) {
+			return label5;
+		} else {
+			return null;
+		}
 	}
 }
