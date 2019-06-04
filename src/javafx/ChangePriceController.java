7f49ab63123263d8e2708c994849a58fd893e049
D074024 <D074024@WDFN34367201A.emea.global.corp.sap>
2019-06-04 15:28:17 +0200

    aaa

--- src/javafx/ChangePriceController.java
+++ src/javafx/ChangePriceController.java
@@ -14,7 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ChangePriceController implements Initializable {
-	
+
 	@FXML
 	TextField newSuperPrice;
 	@FXML
@@ -29,13 +29,10 @@ public class ChangePriceController implements Initializable {
 	TextField newBullPrice;
 	@FXML
 	TextField newPizzaPrice;
-	
+
 	TankModel tankModel = TankModel.getInstance();
 	SalesModel salesModel = SalesModel.getInstance();
-	
-	public AdministrationController aministrationController = AdministrationController.getInstance();
-	
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		newSuperPrice.setPromptText(tankModel.readPrice("Super"));
@@ -46,68 +43,43 @@ public class ChangePriceController implements Initializable {
 		newBullPrice.setPromptText(tankModel.readPrice("Bull"));
 		newPizzaPrice.setPromptText(tankModel.readPrice("Pizza"));
 	}
-	
-	public void changePrices(ActionEvent actionEvent){
-		if(!newSuperPrice.getText().equals("")) {
+
+	public void changePrices(ActionEvent actionEvent) {
+		if (!newSuperPrice.getText().equals("")) {
 			tankModel.getTank("Super").setPricePerLitre(Float.valueOf(newSuperPrice.getText()));
 			tankModel.writePrice("Super", newSuperPrice.getText());
 		}
-		if(!newDieselPrice.getText().equals("")) {
+		if (!newDieselPrice.getText().equals("")) {
 			tankModel.getTank("Diesel").setPricePerLitre(Float.valueOf(newDieselPrice.getText()));
 			tankModel.writePrice("Diesel", newDieselPrice.getText());
 		}
-		if(!newWodkaPrice.getText().equals("")) {
+		if (!newWodkaPrice.getText().equals("")) {
 			salesModel.getProduct("Wodka").setPriceSell(Float.valueOf(newWodkaPrice.getText()));
 			salesModel.writePrice("Wodka", newWodkaPrice.getText());
 		}
-		if(!newFilipPrice.getText().equals("")) {
+		if (!newFilipPrice.getText().equals("")) {
 			salesModel.getProduct("Filip").setPriceSell(Float.valueOf(newFilipPrice.getText()));
 			salesModel.writePrice("Filip", newFilipPrice.getText());
 		}
-		if(!newJupiterPrice.getText().equals("")) {
+		if (!newJupiterPrice.getText().equals("")) {
 			salesModel.getProduct("Jupiter").setPriceSell(Float.valueOf(newJupiterPrice.getText()));
 			salesModel.writePrice("Jupiter", newJupiterPrice.getText());
 		}
-		if(!newBullPrice.getText().equals("")) {
+		if (!newBullPrice.getText().equals("")) {
 			salesModel.getProduct("Bull").setPriceSell(Float.valueOf(newBullPrice.getText()));
 			salesModel.writePrice("Bull", newBullPrice.getText());
 		}
-		if(!newPizzaPrice.getText().equals("")) {
+		if (!newPizzaPrice.getText().equals("")) {
 			salesModel.getProduct("Pizza").setPriceSell(Float.valueOf(newPizzaPrice.getText()));
 			salesModel.writePrice("Pizza", newPizzaPrice.getText());
 		}
-		Stage popup = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-//		refresh();
+		Stage popup = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 		popup.hide();
 	}
-	
+
 	public void cancel(ActionEvent actionEvent) {
-		Stage popup = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+		Stage popup = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 		popup.hide();
 	}
-	
-//	public void refresh() {
-//		administrationController.getSuperBar().setProgress((double) Float.valueOf(tankModel.readFuelLevel("Super")) / tankModel.getTank("Super").getCapacity());
-//		if ((Float.valueOf(tankModel.readFuelLevel("Super")) / tankModel.getTank("Super").getCapacity()) > 0.5) {
-//			superBar.setStyle("-fx-accent: #1ae204");
-//		} else if ((Float.valueOf(tankModel.readFuelLevel("Super")) / tankModel.getTank("Super").getCapacity()) > 0.25) {
-//			superBar.setStyle("-fx-accent: yellow");
-//		} else if ((Float.valueOf(tankModel.readFuelLevel("Super")) / tankModel.getTank("Super").getCapacity()) <= 0.25) {
-//			superBar.setStyle("-fx-accent: red");
-//		}
-//		dieselBar.setProgress((double) Float.valueOf(tankModel.readFuelLevel("Diesel")) / tankModel.getTank("Diesel").getCapacity());
-//		
-//		superPriceLabel.setText(tankModel.readPrice("Super") + " €/L");
-//		dieselPriceLabel.setText(tankModel.readPrice("Diesel") + " €/L");
-//		
-//		superCapLabel.setText(String.valueOf(tankModel.readFuelLevel("Super") + "/" + tankModel.getTank("Super").getCapacity()) + " L");
-//		dieselCapLabel.setText(String.valueOf(tankModel.readFuelLevel("Diesel") + "/" + tankModel.getTank("Diesel").getCapacity()) + " L");
-//		
-//		superSlider.setMax((double)tankModel.getTank("Super").getCapacity() - Double.valueOf(tankModel.readFuelLevel("Super")));
-//		dieselSlider.setMax((double)tankModel.getTank("Diesel").getCapacity() - Double.valueOf(tankModel.readFuelLevel("Diesel")));
-//		superSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-//			superOrder.setText(String.valueOf(superSlider.getValue()));
-//		});
-//	}
-	
+
 }
